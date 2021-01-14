544ab4441131419840a6e867a1e9c2569ecfe1d6
PerU-MoNsteR <71298965+PerU-MoNsteR+@users.noreply.github.com>
2021-01-14 16:01:18 +0000

    perumonster: auto fixes

--- Fonts/salive.py
+++ Fonts/salive.py
@@ -47,27 +47,24 @@ async def _(event):
             )
             await event.delete()
     await event.edit(OUTPUT)
-from telethon import events
-import subprocess
-from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
+
 
 @borg.on(admin_cmd(pattern="salive(.*)"))
 async def _(event):
     if event.fwd_from or event.via_bot_id:
         return
-    DELAY_BETWEEN_EDITS = 0.3
     PROCESS_RUN_TIME = 100
     cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    start_time = time.time() + PROCESS_RUN_TIME
+    time.time() + PROCESS_RUN_TIME
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await process.communicate()
-    e = stderr.decode()
-    
+    stderr.decode()
+
     OUTPUT = f"★彡[ᴀɴɪᴇ]彡★"
     if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
@@ -78,7 +75,7 @@ async def _(event):
                 force_document=True,
                 allow_cache=False,
                 caption=cmd,
-                reply_to=reply_to_id
+                reply_to=reply_to_id,
             )
             await event.delete()
     await event.edit(OUTPUT)
