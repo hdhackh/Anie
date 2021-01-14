62ddad2d81cd011d28bd1da065bfca6e3cc607b6
PerU-MoNsteR <71298965+PerU-MoNsteR+@users.noreply.github.com>
2021-01-14 12:31:46 +0000

    perumonster: auto fixes

--- userbot/plugins/salive.py
+++ userbot/plugins/salive.py
@@ -3,12 +3,10 @@ Syntax: .exec Code"""
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from telethon import events
-import subprocess
-from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
-import io
 import asyncio
+import io
 import time
+
 from uniborg.util import admin_cmd
 
 
@@ -16,19 +14,18 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from or event.via_bot_id:
         return
-    DELAY_BETWEEN_EDITS = 0.3
     PROCESS_RUN_TIME = 100
     cmd = event.pattern_match.group(1)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    start_time = time.time() + PROCESS_RUN_TIME
+    time.time() + PROCESS_RUN_TIME
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
     stdout, stderr = await process.communicate()
-    e = stderr.decode()
-    
+    stderr.decode()
+
     OUTPUT = f"★彡[ᴀɴɪᴇ]彡★"
     if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
@@ -39,7 +36,7 @@ async def _(event):
                 force_document=True,
                 allow_cache=False,
                 caption=cmd,
-                reply_to=reply_to_id
+                reply_to=reply_to_id,
             )
             await event.delete()
     await event.edit(OUTPUT)
