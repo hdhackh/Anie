5aafc484c3db6ba6ca7483fe47c56adf4b04a429
PerU-MoNsteR <71298965+PerU-MoNsteR+@users.noreply.github.com>
2021-01-15 08:50:36 +0000

    perumonster: auto fixes

--- userbot/file_converter.py
+++ userbot/file_converter.py
@@ -6,13 +6,14 @@
 # @ItzSjDude
 #  Added Paste System by @danish_00
 
-import os, requests, re
-import asyncio
-import time
+import os
 from datetime import datetime
 
-from userbot.utils import admin_cmd, sudo_cmd , edit_or_reply
-from userbot import CMD_HELP, bot
+import requests
+
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
+
 
 @borg.on(admin_cmd(pattern=r"open", outgoing=True))
 async def _(event):
@@ -21,15 +22,17 @@ async def _(event):
     c = a.read()
     a.close()
     a = await event.reply("Reading file...")
-    if len(c) >= 4096:            
-            await event.edit("output file too large lemme paste it 😜😜")#hehe
-            out = c
-            url = "https://del.dog/documents"
-            r = requests.post(url, data=out.encode("UTF-8")).json()
-            url = f"https://del.dog/{r['key']}"
-            await event.edit(
-                f" Output file is too large Not supported By Telegram\n**So Pasted to** [Dog Bin]({url}) 😁😁", link_preview=False)            
-            await a.delete()
+    if len(c) >= 4096:
+        await event.edit("output file too large lemme paste it 😜😜")  # hehe
+        out = c
+        url = "https://del.dog/documents"
+        r = requests.post(url, data=out.encode("UTF-8")).json()
+        url = f"https://del.dog/{r['key']}"
+        await event.edit(
+            f" Output file is too large Not supported By Telegram\n**So Pasted to** [Dog Bin]({url}) 😁😁",
+            link_preview=False,
+        )
+        await a.delete()
     else:
         await event.client.send_message(event.chat_id, f"{c}")
         await a.delete()
@@ -53,15 +56,14 @@ async def get(event):
         await event.edit("reply to text message as .doc <file name.extension>")
 
 
+# hehe
 
-#hehe
 
+# .stoi nd .itos made by @danish_00
 
-#.stoi nd .itos made by @danish_00
+# teamcobra
 
-#teamcobra 
-
-#hehe
+# hehe
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
 
@@ -72,33 +74,31 @@ async def danish(hehe):
     if hehe.fwd_from:
         return
     thumb = None
-    reply_to_id = hehe.message.id
+    hehe.message.id
     if hehe.reply_to_msg_id:
-        reply_to_id = hehe.reply_to_msg_id
+        hehe.reply_to_msg_id
     cobra = await edit_or_reply(hehe, "Converting.....")
-    
-  
+
     input_str = "dc.jpeg"
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if cobra.reply_to_msg_id:
-        start = datetime.now()
+        datetime.now()
         file_name = input_str
         reply_message = await cobra.get_reply_message()
-      
+
         to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
         downloaded_file_name = os.path.join(to_download_directory, file_name)
         downloaded_file_name = await hehe.client.download_media(
-            reply_message,
-            downloaded_file_name
+            reply_message, downloaded_file_name
         )
-      
+
         try:
             thumb = await reply_message.download_media(thumb=-1)
         except Exception:
             thumb = thumb
         if os.path.exists(downloaded_file_name):
-            
+
             dc = await hehe.client.send_file(
                 hehe.chat_id,
                 downloaded_file_name,
@@ -106,10 +106,9 @@ async def danish(hehe):
                 supports_streaming=True,
                 allow_cache=False,
                 reply_to=reply_message,
-                thumb=thumb
-                
+                thumb=thumb,
             )
-            
+
             os.remove(downloaded_file_name)
             await cobra.delete()
         else:
@@ -117,43 +116,41 @@ async def danish(hehe):
     else:
         await cobra.edit("reply to a non animated sticker")
 
-  
-  
-  #hehe
-  
+
+# hehe
+
+
 @borg.on(admin_cmd(pattern="itos"))
 @borg.on(sudo_cmd(pattern="itos", allow_sudo=True))
 async def teamcobra(hehe):
     if hehe.fwd_from:
         return
     thumb = None
-    reply_to_id = hehe.message.id
+    hehe.message.id
     if hehe.reply_to_msg_id:
-        reply_to_id = hehe.reply_to_msg_id
+        hehe.reply_to_msg_id
     cobra = await edit_or_reply(hehe, "Converting.....")
-    
-  
+
     input_str = "dc.webp"
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if cobra.reply_to_msg_id:
-        start = datetime.now()
+        datetime.now()
         file_name = input_str
         reply_message = await cobra.get_reply_message()
-      
+
         to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
         downloaded_file_name = os.path.join(to_download_directory, file_name)
         downloaded_file_name = await hehe.client.download_media(
-            reply_message,
-            downloaded_file_name
+            reply_message, downloaded_file_name
         )
-      
+
         try:
             thumb = await reply_message.download_media(thumb=-1)
         except Exception:
             thumb = thumb
         if os.path.exists(downloaded_file_name):
-            
+
             dc = await hehe.client.send_file(
                 hehe.chat_id,
                 downloaded_file_name,
@@ -161,10 +158,9 @@ async def teamcobra(hehe):
                 supports_streaming=True,
                 allow_cache=False,
                 reply_to=reply_message,
-                thumb=thumb
-                
+                thumb=thumb,
             )
-            
+
             os.remove(downloaded_file_name)
             await cobra.delete()
         else:
@@ -172,7 +168,7 @@ async def teamcobra(hehe):
     else:
         await cobra.edit("reply to a non animated sticker")
 
-  
+
 CMD_HELP.update(
     {
         "fileconverter": "PLUGIN NAME : fileconverter\
