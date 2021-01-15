06c81bf0475e48ca36c7e9abd7914dcf57bf6fd5
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-15 13:42:58 +0530

    Add files via upload

---
+++ userbot/plugins/crypto.py
@@ -0,0 +1,90 @@
+# Made By @RoseloverX Kang With Credits Else Gey [Modified from @xditya's Plugin]
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+naam = str(ALIVE_NAME)
+
+bot = "@HeathonCryptoBot"
+
+
+@borg.on(admin_cmd("btc ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sysarg = event.pattern_match.group(1)
+    if sysarg == "":
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/btc ")
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("Error: unblock @HeathonCryptoBot and retry!")
+    elif "@" in sysarg:
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/btc " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("Error: unblock @HeathonCryptoBot and try again!")
+    elif "" in sysarg:
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/btc " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("Error: unblock @HeathonCryptoBot `and try again!")
+
+
+@borg.on(admin_cmd("crypto ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sysarg = event.pattern_match.group(1)
+    if sysarg == "":
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/crypto ")
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("Error: unblock @HeathonCryptoBot and retry!")
+    elif "@" in sysarg:
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/crypto " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("Error: unblock @HeathonCryptoBot and try again!")
+    elif "" in sysarg:
+        async with borg.conversation(bot) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/crypto " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("Error: unblock @HeathonCryptoBot `and try again!")
