6d62531550a0f5f8f68bb31534746fd0e29002b1
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-15 11:32:39 +0530

    Update _core.py

--- userbot/plugins/_core.py
+++ userbot/plugins/_core.py
@@ -9,7 +9,7 @@ from userbot.utils import edit_or_reply as eor
 from userbot.utils import load_module, remove_plugin, sudo_cmd
 
 DELETE_TIMEOUT = 3
-thumb_image_path = "./Fonts/IMG_20201005_150245_168.jpg"
+thumb_image_path = "./Fonts/wp1909427-amoled-wallpapers.png"
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Anie"
 
 
