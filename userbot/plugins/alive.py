62edb1711e050cd35a10f4d2a2e99222bc3388ab
PerU-MoNsteR <71298965+PerU-MoNsteR+@users.noreply.github.com>
2021-01-15 12:03:09 +0000

    perumonster: auto fixes

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,21 +1,16 @@
-# Thanks to Sipak bro and Aryan.. 
+# Thanks to Sipak bro and Aryan..
 # animation Idea by @ItzSipak && @Hell boy_pikachu
 # Made by @hellboi_atul ....and thanks to @Crackexy for the logos...
 # Kang with credits else gay...
 import asyncio
-import os
-import requests
-from PIL import Image
-from io import BytesIO
-import random
-from telethon import events, version
-from userbot.utils import admin_cmd, sudo_cmd
 import time
-from datetime import datetime
-from userbot.__init__ import StartTime
+
 from userbot import ALIVE_NAME
-from telethon.tl.types import ChannelParticipantsAdmins
+from userbot.__init__ import StartTime
+from userbot.utils import admin_cmd, sudo_cmd
+
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ǟռɨɛ"
+
 
 def get_readable_time(seconds: int) -> str:
     count = 0
@@ -44,9 +39,10 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
+
 uptime = get_readable_time((time.time() - StartTime))
 
-# Thanks to Sipak bro and Aryan.. 
+# Thanks to Sipak bro and Aryan..
 # animation Idea by @ItzSipak && @Hell boy_pikachu
 # Made by @hellboi_atul ....and thanks to @Crackexy for the logos...
 # Kang with credits else gay...
@@ -64,42 +60,38 @@ file4 = "https://telegra.ph/file/9271370fd1f5dd877388b.jpg"
 
 @borg.on(admin_cmd(pattern=r"ae"))
 @borg.on(sudo_cmd(pattern=r"ae", allow_sudo=True))
-
 async def hmm(yes):
-    chat = await yes.get_chat()
+    await yes.get_chat()
     global ghanti
     ghanti = borg.uid
-    await yes.delete() 
+    await yes.delete()
     pm_caption = "**↤↤↤↤↤ ★彡ᴀɴɪᴇ]彡★ ↦↦↦↦↦**\n\n"
     pm_caption += "**★ǟʟʟ ֆʏֆȶɛʍֆ ǟʀɛ օռ**\n\n"
     pm_caption += "★ǟɮօʊȶ ʍʏ ֆʏֆȶɛʍ\n\n"
     pm_caption += f"★ȶɛʟɛȶɦօռ ʋɛʀֆɨօռ★ 1.17.8\n"
     pm_caption += "★ʟɨƈɛռֆɛ★  [ǟռɨɛ](https://github.com/Amarnathcdj)\n"
     pm_caption += "★ƈօքʏʀɨɢɦȶ ɮʏ★ [ǟռɨɛ u̴b̴](https://github.com/Amarnathcdj/Anie)\n"
-    pm_caption +=f"★ʊքȶɨʍɛ★ {uptime}\n\n"
+    pm_caption += f"★ʊքȶɨʍɛ★ {uptime}\n\n"
     pm_caption += f"★ʍʏ ʍǟֆȶɛʀ★ [{DEFAULTUSER}](tg://user?id={ghanti})\n"
-    on = await borg.send_file(yes.chat_id, file=file1,caption=pm_caption)
+    on = await borg.send_file(yes.chat_id, file=file1, caption=pm_caption)
 
     await asyncio.sleep(edit_time)
-    ok = await borg.edit_message(yes.chat_id, on, file=file2) 
+    ok = await borg.edit_message(yes.chat_id, on, file=file2)
 
     await asyncio.sleep(edit_time)
     ok2 = await borg.edit_message(yes.chat_id, ok, file=file3)
 
     await asyncio.sleep(edit_time)
     ok3 = await borg.edit_message(yes.chat_id, ok2, file=file1)
-    
+
     await asyncio.sleep(edit_time)
     ok4 = await borg.edit_message(yes.chat_id, ok3, file=file3)
-    
+
     await asyncio.sleep(edit_time)
     ok5 = await borg.edit_message(yes.chat_id, ok4, file=file2)
-    
+
     await asyncio.sleep(edit_time)
     ok6 = await borg.edit_message(yes.chat_id, ok5, file=file1)
-    
+
     await asyncio.sleep(edit_time)
     ok7 = await borg.edit_message(yes.chat_id, ok6, file=file4)
-
-    
-
