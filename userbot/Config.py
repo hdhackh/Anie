5fd441615a92f6c6b98c0b84da0ef56363587ef7
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-29 21:02:13 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -128,10 +128,10 @@ if ENV:
         CHROME_BIN = os.environ.get("CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
         CHROME_DRIVER = os.environ.get(
         "CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver"
-    )
-    GOOGLE_CHROME_BIN = os.environ.get(
-        "GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome"
-    )
+        )
+        GOOGLE_CHROME_BIN = os.environ.get(
+          "GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome"
+        )
         # Google Drive plugin
         G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
         G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
