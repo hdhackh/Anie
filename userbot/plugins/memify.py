fccf48922ed0d510d9fb9da847eba4c5a64b353c
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-09 11:20:26 +0530

    Update memify.py

--- userbot/plugins/memify.py
+++ userbot/plugins/memify.py
@@ -10,8 +10,6 @@ from . import (
     LOGS,
     add_frame,
     asciiart,
-    cat_meeme,
-    cat_meme,
     convert_toimage,
     convert_tosticker,
     crop,
@@ -32,7 +30,84 @@ def random_color():
         "#" + "".join(random.choice("0123456789ABCDEF") for j in range(6))
         for i in range(number_of_colors)
     ]
+import asyncio
+import os
+import shlex
+from os import getcwd
+from os.path import basename, join
+from textwrap import wrap
+from typing import Optional, Tuple
 
+import numpy as np
+
+async def cat_meeme(upper_text, lower_text, CNG_FONTS, picture_name, endname):
+    main_image = catimage(filename=picture_name)
+    main_image.resize(
+        1024, int(((main_image.height * 1.0) / (main_image.width * 1.0)) * 1024.0)
+    )
+    upper_text = "\n".join(wrap(upper_text, get_warp_length(main_image.width))).upper()
+    lower_text = "\n".join(wrap(lower_text, get_warp_length(main_image.width))).upper()
+    lower_margin = MARGINS[lower_text.count("\n")]
+    text_draw = Drawing()
+    text_draw.font = join(getcwd(), CNG_FONTS)
+    text_draw.font_size = 100
+    text_draw.text_alignment = "center"
+    text_draw.stroke_color = Color("black")
+    text_draw.stroke_width = 3
+    text_draw.fill_color = Color("white")
+    if upper_text:
+        text_draw.text((main_image.width) // 2, 80, upper_text)
+    if lower_text:
+        text_draw.text(
+            (main_image.width) // 2, main_image.height - lower_margin, lower_text
+        )
+    text_draw(main_image)
+    main_image.save(filename=endname)
+
+async def cat_meme(CNG_FONTS, topString, bottomString, filename, endname):
+    img = Image.open(filename)
+    imageSize = img.size
+    # find biggest font size that works
+    fontSize = int(imageSize[1] / 5)
+    font = ImageFont.truetype(CNG_FONTS, fontSize)
+    topTextSize = font.getsize(topString)
+    bottomTextSize = font.getsize(bottomString)
+    while topTextSize[0] > imageSize[0] - 20 or bottomTextSize[0] > imageSize[0] - 20:
+        fontSize -= 1
+        font = ImageFont.truetype(CNG_FONTS, fontSize)
+        topTextSize = font.getsize(topString)
+        bottomTextSize = font.getsize(bottomString)
+
+    # find top centered position for top text
+    topTextPositionX = (imageSize[0] / 2) - (topTextSize[0] / 2)
+    topTextPositionY = 0
+    topTextPosition = (topTextPositionX, topTextPositionY)
+
+    # find bottom centered position for bottom text
+    bottomTextPositionX = (imageSize[0] / 2) - (bottomTextSize[0] / 2)
+    bottomTextPositionY = imageSize[1] - bottomTextSize[1]
+    bottomTextPosition = (bottomTextPositionX, bottomTextPositionY)
+    draw = ImageDraw.Draw(img)
+    # draw outlines
+    # there may be a better way
+    outlineRange = int(fontSize / 15)
+    for x in range(-outlineRange, outlineRange + 1):
+        for y in range(-outlineRange, outlineRange + 1):
+            draw.text(
+                (topTextPosition[0] + x, topTextPosition[1] + y),
+                topString,
+                (0, 0, 0),
+                font=font,
+            )
+            draw.text(
+                (bottomTextPosition[0] + x, bottomTextPosition[1] + y),
+                bottomString,
+                (0, 0, 0),
+                font=font,
+            )
+    draw.text(topTextPosition, topString, (255, 255, 255), font=font)
+    draw.text(bottomTextPosition, bottomString, (255, 255, 255), font=font)
+    img.save(endname)
 
 CNG_FONTS = "userbot/helpers/styles/impact.ttf"
 FONTS = "1. `ProductSans-BoldItalic.ttf`\n2. `ProductSans-Light.ttf`\n3. `RoadRage-Regular.ttf`\n4. `digital.ttf`\n5. `impact.ttf`"
