ec008c81175cc604b58ed502dbc19a5842b84459
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-25 13:05:18 +0530

    Update local_config.py

--- local_config.py
+++ local_config.py
@@ -9,6 +9,7 @@ from telethon.tl.types import ChatBannedRights
 class Config((object)):
     LOGGER = True
     # Get this value from my.telegram.org! Please do not steal
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
     SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get("SCREEN_SHOT_LAYER_ACCESS_KEY", None)
