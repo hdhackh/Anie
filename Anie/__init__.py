bedf9b2d9d3a7665a22fdc53fdddc8560c693abf
Amarnath C <amarnathcharichilil@gmail.com>
2021-04-01 20:25:08 +0530

    Create __init__.py

---
+++ Anie/__init__.py
@@ -0,0 +1,64 @@
+import logging
+import os
+import sys
+import time
+import os
+import urllib.parse as urlparse
+import json
+from logging import basicConfig
+from logging import DEBUG
+from logging import getLogger
+from logging import INFO
+
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+StartTime = time.time()
+CMD_LIST = {}
+CMD_HELP = {}
+LOAD_PLUG = {}
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+LOGGER = logging.getLogger(__name__)
+ENV = bool(os.environ.get("ENV", True))
+
+if ENV:
+    TOKEN = os.environ.get("TOKEN", None)
+    OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+    API_KEY = os.environ.get("API_KEY", None)
+    API_HASH = os.environ.get("API_HASH", None)
+    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+    DB_URI = os.environ.get("DATABASE_URL")
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    WOLFRAM_ID = os.environ.get("WOLFRAM_ID", None)
+    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", None)
+    tbot = TelegramClient(None, API_KEY, API_HASH)
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL", None)
+    CONSOLE_LOGGER_VERBOSE = os.environ.get("CONSOLE_LOGGER_VERBOSE", "False")
+    BOT_ID = int(os.environ.get("BOT_ID", None))    
+    if CONSOLE_LOGGER_VERBOSE:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=DEBUG
+        )
+    else:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
+        )
+    LOGS = getLogger(__name__)
+
+
+if STRING_SESSION:
+    session_name = str(STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), API_KEY, API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, API_HASH, API_HASH)
+
+
